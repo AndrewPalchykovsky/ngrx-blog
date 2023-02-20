59a2125854635dd5f4b26c0c34f34e543773a418
Andrew Palchykovsky <dangerok2@gmail.com>
2023-02-20 21:42:12 +0200

    feat: add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import {NgModule} from '@angular/core';
+import {isDevMode, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {StoreModule} from '@ngrx/store';
 import {AuthModule} from './auth/auth/auth.module';
 
 @NgModule({
@@ -12,7 +13,12 @@ import {AuthModule} from './auth/auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
