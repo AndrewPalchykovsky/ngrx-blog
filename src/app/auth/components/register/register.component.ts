2e814ed6d1c497e819be6535e3558f8b6518c7c3
Andrew Palchykovsky <dangerok2@gmail.com>
2023-02-21 19:01:05 +0200

    feat: register service

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -5,6 +5,8 @@ import {Observable} from 'rxjs';
 
 import {registerAction} from 'src/app/auth/store/actions';
 import {isSubmittingSelector} from 'src/app/auth/store/selectors';
+import {AuthService} from 'src/app/auth/services/auth.service';
+import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface';
 
 @Component({
   selector: 'mc-register',
@@ -15,7 +17,11 @@ export class RegisterComponent implements OnInit {
   form: FormGroup;
   isSubmitting$: Observable<boolean>;
 
-  constructor(private fb: FormBuilder, private store: Store) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -27,7 +33,6 @@ export class RegisterComponent implements OnInit {
   }
 
   initializeForm(): void {
-    console.log('initializeForm');
     this.form = this.fb.group({
       username: ['', Validators.required],
       email: ['', Validators.required],
@@ -36,7 +41,11 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log('submit', this.form.value, this.form.valid);
     this.store.dispatch(registerAction(this.form.value));
+    this.authService
+      .register(this.form.value)
+      .subscribe((currentUser: CurrentUserInterface) => {
+        console.log(currentUser);
+      });
   }
 }
