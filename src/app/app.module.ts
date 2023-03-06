0558a7039e14c50333c72f30ee729bb888fed8b5
Andrew Palchykovsky <dangerok2@gmail.com>
2023-03-06 21:49:39 +0200

    feat: top bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {AuthModule} from './auth/auth.module';
+import {TopBarComponent} from './shared/modules/topBar/topBar.component';
 
 @NgModule({
   declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -23,8 +26,7 @@ import {AuthModule} from './auth/auth.module';
       autoPause: true,
     }),
     EffectsModule.forRoot([]),
+    TopBarComponent,
   ],
-  providers: [],
-  bootstrap: [AppComponent],
 })
 export class AppModule {}
