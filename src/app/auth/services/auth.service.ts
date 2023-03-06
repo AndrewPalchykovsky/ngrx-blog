7bcb956371d31439057420b8e4b90bd3f4bf736e
Andrew Palchykovsky <dangerok2@gmail.com>
2023-03-06 21:04:20 +0200

    feat: login store

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -5,15 +5,27 @@ import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface';
 import {RegisterRequestInterface} from 'src/app/auth/types/registerRequest.interface';
 import {environment} from 'src/environments/environment';
 import {AuthResponseInterface} from 'src/app/auth/types/authResponse.interface';
+import {LoginRequestInterface} from 'src/app/auth/types/loginRequest.interface';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  getUser(response: AuthResponseInterface): CurrentUserInterface {
+    return response.user;
+  }
 
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
     const url = environment.apiUrl + '/users';
     return this.http
       .post<AuthResponseInterface>(url, data)
       .pipe(map((response: AuthResponseInterface) => response.user));
+  }
+
+  login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/users/login';
+    return this.http
+      .post<AuthResponseInterface>(url, data)
+      .pipe(map(this.getUser));
   }
 }
